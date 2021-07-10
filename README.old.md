c743706c1286d49bb171e70c1d6df7ee67b3e826
EdoSlash999 <edo_tz_@hotmail.com>
2021-07-10 19:14:41 +0200

    calculator

---
+++ README.old.md
@@ -0,0 +1,11 @@
+# reverse-polish-notation-calculator
+
+Built With:
+HTML
+CSS
+JS
+React JS
+
+1. Clone project from gitHub on your local machine.
+2. In VS code terminal type "npm install" to install all necessary packages for this project.
+3. After installing you can type "npm start" to run project on localhost 3000;
---
+++ src/Calculator.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+
+function Calculator() {
+  const [input, setInput] = useState("");
+  const [msg, setMsg] = useState("");
+
+  const compute = () => {
+    if (input === "") {
+      setMsg("You did not enter anything");
+    } else {
+      if (input.match(/^[a-zA-Z]+$/)) {
+        setMsg("Only numbers and aritmetic operators are valid!");
+      } else {
+        const myArr = input.split(",");
+        const numArr = [];
+        let tempA = 0;
+        let tempB = 0;
+        for (let i = 0; i < myArr.length; i++) {
+          if (parseFloat(myArr[i])) {
+            numArr.push(parseFloat(myArr[i]));
+            tempA++;
+          } else {
+            tempB++;
+            let a = numArr.pop();
+            let b = numArr.pop();
+            if (myArr[i] === "+") {
+              numArr.push(a + b);
+            } else if (myArr[i] === "-") {
+              numArr.push(b - a);
+            } else if (myArr[i] === "*") {
+              numArr.push(a * b);
+            } else if (myArr[i] === "/") {
+              numArr.push(b / a);
+            } else if (myArr[i] === "^") {
+              numArr.push(Math.pow(b, a));
+            }
+          }
+        }
+        if (tempA - 1 === tempB) {
+          setInput(input + " = " + numArr);
+        } else {
+          setMsg("Something went wrong! Check the number of OPERANDS!");
+        }
+      }
+    }
+  };
+
+  const onChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const clear = () => {
+    setInput("");
+    setMsg("");
+  };
+
+  return (
+    <div>
+      <input
+        type='text'
+        value={input}
+        onChange={onChange}
+        className='input-form'
+      ></input>
+      <button type='button' onClick={clear} className='clear'>
+        Clear
+      </button>
+      <button type='button' className='compute' onClick={compute}>
+        Compute
+      </button>
+
+      <div className='error'>
+        <p className='msg'>{msg}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
